974c611619d089971053964e532c2ce6ffe2bff6
serenaf <serena@intercom.io>
2018-01-15 22:41:41 +0000

    fixing some styling

--- app/components/deprecation-article.js
+++ app/components/deprecation-article.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  tagName: '',
+  idForTitle: computed('model.title', function() {
+    return `toc_${this.get('model.title')}`;
+  }),
+  idForUntil: computed('model.until', function() {
+    return `toc_until-${this.get('model.until')}`;
+  }),
 });
