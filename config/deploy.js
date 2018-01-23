58faf8a1e741f823e686aa6405de5ece2b2e0c47
Chris Manson <chris@stonecircle.io>
2018-01-23 08:52:30 +0000

    using ember guides-app style deployment includeing prember

--- config/deploy.js
+++ config/deploy.js
@@ -1,40 +1,50 @@
-/* eslint-env node */
-'use strict';
+/* jshint node: true */
 
-const gcloudProjectId = 'stone-circle-internal';
+let credentials;
+
+try {
+  credentials = require('./credentials.json');
+} catch (e) {
+  credentials = {};
+}
 
 module.exports = function(deployTarget) {
-  let ENV = {
-    gcloudUrl: 'https://storage.googleapis.com',
-    'gcloud-storage': {
-      projectId: gcloudProjectId,
+  var ENV = {
+    build: {
+      environment: "production"
     },
-    'gcs-index': {
-      projectId: gcloudProjectId,
+    pipeline: {
+      // This setting runs the ember-cli-deploy activation hooks on every deploy
+      // which is necessary in order to run ember-cli-deploy-cloudfront.
+      // To disable CloudFront invalidation, remove this setting or change it to `false`.
+      // To disable ember-cli-deploy-cloudfront for only a particular environment, add
+      // `ENV.pipeline.activateOnDeploy = false` to an environment conditional below.
+      activateOnDeploy: true
     },
-    build: {}
-    // include other plugin configuration that applies to all deploy targets here
+    "revision-data": {
+      "type": "version-commit"
+    },
+    s3: {
+      filePattern: '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2,json,html}',
+    },
+    's3-index': {
+      allowOverwrite: true
+    },
   };
 
-  if (deployTarget === 'development') {
-    ENV.build.environment = 'development';
-    // configure other plugins for development deploy target here
-  }
-
-  if (deployTarget === 'staging') {
-    ENV.build.environment = 'production';
-    // configure other plugins for staging deploy target here
-  }
-
   if (deployTarget === 'production') {
-    ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
 
-    const productionBucket = 'emberdeprecationsapp.stonecircle.io';
+    const bucket = 'emberdeprecationsapp.stonecircle.io';
+    const region = 'eu-west-1'
 
-    ENV['gcloud-storage'].bucket = productionBucket;
-    ENV['gcloud-storage'].filePattern = '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2,json}';
-    ENV['gcs-index'].bucket = productionBucket;
+    ENV.s3.accessKeyId = credentials.key || process.env.AWS_KEY;
+    ENV.s3.secretAccessKey = credentials.secret || process.env.AWS_SECRET;
+    ENV.s3.bucket = bucket;
+    ENV.s3.region = region;
+    ENV["s3-index"].accessKeyId = credentials.key || process.env.AWS_KEY;
+    ENV["s3-index"].secretAccessKey = credentials.secret || process.env.AWS_SECRET;
+    ENV["s3-index"].bucket = bucket;
+    ENV["s3-index"].region = region;
   }
 
   // Note: if you need to build some configuration asynchronously, you can return
