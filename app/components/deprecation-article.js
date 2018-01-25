eb26d8ececbf4bf9ffc2355d286addfadb07250b
Chris Manson <chris@stonecircle.io>
2018-01-25 11:11:32 +0000

    making the toc links work 🎉

--- app/components/deprecation-article.js
+++ app/components/deprecation-article.js
@@ -9,12 +9,6 @@ export default Component.extend({
   idForUntil: computed('model.until', function() {
     return `toc_until-${this.get('model.until')}`;
   }),
-  idForDeprecation: computed('model.id', function() {
-    let idString = this.get('model.id');
-    idString = idString.replace(/\s+/g, '');
-    let replacedIdString = idString.replace(/\.|,/g, '-');
-    return `toc_id-${replacedIdString}`;
-  }),
 
   didRender() {
     let nodeList = this.$('pre:not(.no-line-numbers) > code');
