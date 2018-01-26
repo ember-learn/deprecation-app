3bff15e07dba6ce71eb4ed3586f73d15bbfb8a45
Jen Weber <j@jenweber.me>
2018-01-26 00:07:24 -0500

    sync up header/footer with latest design work #26

--- app/components/navigation-footer.js
+++ app/components/navigation-footer.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['container']
+  classNames: ['container'],
+  currentYear: new Date().getFullYear()
 });
