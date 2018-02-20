65d0e588345afb3f24096f434d6633f848cae2a0
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2018-02-21 00:10:11 +0530

    Conditional deploy

--- config/deploy.js
+++ config/deploy.js
@@ -9,8 +9,10 @@ module.exports = function(deployTarget) {
   if (deployTarget === 'production') {
     ENV.build.environment = 'production';
     let fastlyDomain = process.env.FASTLY_DOMAIN;
-    process.env.API_HOST_URL = `https://${fastlyDomain}`;
-    process.env.CDN_URL = `https://${fastlyDomain}/`
+    if (fastlyDomain) {
+      process.env.API_HOST_URL = `https://${fastlyDomain}`;
+      process.env.CDN_URL = `https://${fastlyDomain}/`
+    }
   }
 
   return ENV;
