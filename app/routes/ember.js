be2ab2fa638205161767e964fe26256a1f917562
Chris Manson <chris@stonecircle.io>
2018-02-24 21:49:54 +0000

    removing redirect to make 100% sure that we are not messing up any links

--- app/routes/ember.js
+++ app/routes/ember.js
@@ -1,7 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  redirect(params) {
-    this.transitionTo('show', 'ember', params.version);
+  controllerName: 'show',
+  templateName: 'show',
+  model(params) {
+    return this.store.query('content', {
+      path: 'ember',
+      version: params.version,
+    })
   }
 });
