ed10148efd35861823f52ccaea884fc5eeb24a93
Sivakumar Kailasam <siva@rubberduck.engineering>
2018-05-28 21:13:39 +0530

    Fix eslint errors

--- app/components/deprecation-article.js
+++ app/components/deprecation-article.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import { get, computed } from '@ember/object';
+import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 import $ from 'jquery';
 
