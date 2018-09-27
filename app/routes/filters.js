af5e821724daa350679da5f93e025361d225bae3
Chris Freeman <cfreeman.code@gmail.com>
2018-09-27 15:14:33 -0500

    comments and cleanup

--- app/routes/filters.js
+++ app/routes/filters.js
@@ -4,6 +4,12 @@ import { all } from 'rsvp';
 
 export default Route.extend({
   model() {
+    // Since we currently load the deprecation files in project/version buckets
+    // (e.g. ember-data-2.x), we need to instead get ahold of all of the deprecations at once
+    // so we can do more fine-grained filtering.
+
+    // Read the `contentFolders` config used to serve all the markdown files as static JSON assets
+    // and transform them in `store.query` param objects
     let contentFolders = config.contentFolders.map(folder => {
       let [ path, version ] = folder.split('/');
 
@@ -13,6 +19,8 @@ export default Route.extend({
       };
     });
 
+    // Fire off queries for all the different project/version combinations.
+    // These will be flattened and filtered in the controller.
     let queryObjectPromises = contentFolders.map(version => {
       return this.store.query('content', version);
     });
--- tests/acceptance/filtering-by-versions-test.js
+++ tests/acceptance/filtering-by-versions-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, findAll, pauseTest } from '@ember/test-helpers';
+import { visit, currentURL, findAll } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { select } from 'deprecation-app/tests/helpers/x-select';
 
