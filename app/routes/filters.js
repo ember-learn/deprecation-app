009c0f4ead337a05798e0f47b2f9adc07c8ec41a
Chris Freeman <cfreeman.code@gmail.com>
2018-09-27 13:32:13 -0500

    filter UI PoC

---
+++ app/routes/filters.js
@@ -0,0 +1,22 @@
+import Route from '@ember/routing/route';
+import config from 'deprecation-app/config/environment';
+import { all, map } from 'rsvp';
+
+export default Route.extend({
+  model() {
+    let contentFolders = config.contentFolders.map(folder => {
+      let [ path, version ] = folder.split('/');
+
+      return {
+        path,
+        version: `${version}.x`
+      };
+    });
+
+    let queryObjectPromises = contentFolders.map(version => {
+      return this.store.query('content', version);
+    });
+
+    return all(queryObjectPromises);
+  }
+});
