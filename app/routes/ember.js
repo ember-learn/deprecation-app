a72a4b54d6618161abaed2515be3dc507a3283b4
Serena Fritsch <serena@intercom.io>
2019-04-18 20:38:02 +0100

    fix the unneccessary files

--- app/routes/ember.js
+++ app/routes/ember.js
@@ -1,3 +1,12 @@
-import EmberRoute from './deprecations/ember';
+import Route from '@ember/routing/route';
 
-export default EmberRoute;
+export default Route.extend({
+  controllerName: 'show',
+  templateName: 'show',
+  model(params) {
+    return this.store.query('content', {
+      path: 'ember',
+      version: params.version,
+    })
+  }
+});
