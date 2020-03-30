93f4d42027e40982e21068483ad2a9813f2010e5
Jared Galanis <jaredgalanis@gmail.com>
2020-03-30 09:23:27 -0400

    fix tests

--- app/components/table-of-contents.js
+++ app/components/table-of-contents.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import { set, computed } from '@ember/object';
+import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { later } from '@ember/runloop';
 
