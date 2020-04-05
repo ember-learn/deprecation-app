a1b593eb5fe11815440977ab784e94b9729adf69
Jared Galanis <jaredgalanis@gmail.com>
2020-04-05 12:39:47 -0400

    experimenting w tests

--- tests/acceptance/visual-regression-test.js
+++ tests/acceptance/visual-regression-test.js
@@ -7,9 +7,7 @@ module("Acceptance | visual regression", function(hooks) {
   setupApplicationTest(hooks);
 
   let pagesToLookup = {
-    "home-page": "/",
     "ember-1.x": "/v1.x",
-    "ember-2.x": "/v2.x",
     "ember-3.x": "/v3.x",
     "ember-data-2.x": "/ember-data/v2.x",
     "ember-cli-2.x": "/ember-cli/v2.x"
@@ -18,9 +16,21 @@ module("Acceptance | visual regression", function(hooks) {
   Object.keys(pagesToLookup).map(page => {
     test(`visiting ${page}`, async function(assert) {
       await visit(pagesToLookup[page]);
-      await waitFor('[data-test-toc-list-item]');
+      await waitFor('[data-test-toc-list-item]', { timeout: 2000 });
       await percySnapshot(page);
       assert.expect(0);
     });
   });
+
+  test('visit home page', async function(assert) {
+    await visit('/');
+    await percySnapshot('home-page');
+    assert.expect(0);
+  });
+
+  test('visit ember-2.x', async function(assert) {
+    await visit('/v2.x');
+    await percySnapshot('ember-2.x');
+    assert.expect(0);
+  })
 });
