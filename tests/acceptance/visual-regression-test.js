9a8951982e520c0cf75f6d8cf4ad404b9f8112b7
Jared Galanis <jaredgalanis@gmail.com>
2020-04-05 14:10:19 -0400

    rework acceptance test

--- tests/acceptance/visual-regression-test.js
+++ tests/acceptance/visual-regression-test.js
@@ -1,63 +1,66 @@
-import { visit, waitFor } from "@ember/test-helpers";
+import { click, visit, waitFor } from "@ember/test-helpers";
 import { percySnapshot } from "ember-percy";
 import { setupApplicationTest } from "ember-qunit";
 import { module, test } from "qunit";
-import lolex from 'lolex';
 
 module("Acceptance | visual regression", function(hooks) {
   setupApplicationTest(hooks);
 
-  hooks.beforeEach(function() {
-    this.clock = lolex.install();
-  });
-
-  hooks.afterEach(function() {
-    this.clock.uninstall();
-  });
-
-  let pagesToLookup = {
-    "ember-1.x": "/v1.x",
-    "ember-3.x": "/v3.x",
-    "ember-data-2.x": "/ember-data/v2.x",
-    "ember-cli-2.x": "/ember-cli/v2.x"
-  };
-
-  Object.keys(pagesToLookup).map(page => {
-    test(`visiting ${page}`, async function(assert) {
-      await visit(pagesToLookup[page]);
-      await waitFor('[data-test-toc-list-item]', { timeout: 2000 });
-
-      this.clock.setTimeout(async function () {
-        await percySnapshot(page);
-        assert.expect(0);
-
-      }, 2000);
-      this.clock.tick(2000);
-
-    });
-  });
-
-  test('visit home page', async function(assert) {
+  test('visit all the pages', async function(assert) {
     await visit('/');
 
-    this.clock.setTimeout(async function () {
-      await percySnapshot('home-page');
-      assert.expect(0);
+    await percySnapshot('home-page');
 
-    }, 2000);
-    this.clock.tick(2000);
+    await click('[data-test-ember-1-link] > a');
 
+    await waitFor('[data-test-deprecations-added-in]');
+    assert.dom('[data-test-deprecations-added-in]').includesText('Deprecations Added in Ember 1.x');
+
+    await percySnapshot('ember-1.x');
+
+    await click ('[data-test-main-deprecations-link]');
+
+    await click('[data-test-ember-2-link] > a');
+
+    await waitFor('[data-test-deprecations-added-in]');
+    assert.dom('[data-test-deprecations-added-in]').includesText('Deprecations Added in Ember 2.x');
+
+    await percySnapshot('ember-2.x');
+
+    await click ('[data-test-main-deprecations-link]');
+
+    await click('[data-test-ember-data-2-link] > a');
+
+    await waitFor('[data-test-deprecations-added-in]');
+    assert.dom('[data-test-deprecations-added-in]').includesText('Deprecations Added in Ember Data 2.x');
+
+    await percySnapshot('ember-data-2.x');
+
+    await click ('[data-test-main-deprecations-link]');
+
+    await click('[data-test-ember-cli-2-link] > a');
+
+    await waitFor('[data-test-deprecations-added-in]');
+    assert.dom('[data-test-deprecations-added-in]').includesText('Deprecations Added in Ember CLI 2.x');
+
+    await percySnapshot('ember-cli-2.x');
+
+    await click ('[data-test-main-deprecations-link]');
+
+    await click('[data-test-ember-3-link] > a');
+
+    await waitFor('[data-test-deprecations-added-in]');
+    assert.dom('[data-test-deprecations-added-in]').includesText('Deprecations Added in Ember 3.x');
+
+    await percySnapshot('ember-3.x');
+
+    await click ('[data-test-main-deprecations-link]');
+
+    await click('[data-test-ember-data-3-link] > a');
+
+    await waitFor('[data-test-deprecations-added-in]');
+    assert.dom('[data-test-deprecations-added-in]').includesText('Deprecations Added in Ember Data 3.x');
+
+    await percySnapshot('ember-data-3.x');
   });
-
-  test('visit ember-2.x', async function(assert) {
-    await visit('/v2.x');
-
-    this.clock.setTimeout(async function () {
-      await percySnapshot('ember-2.x');
-      assert.expect(0);
-
-    }, 2000);
-    this.clock.tick(2000);
-
-  })
 });
