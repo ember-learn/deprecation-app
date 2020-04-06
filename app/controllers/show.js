0d90d7c25c3b292320d5265a5b3907635dc66fce
Jared Galanis <jaredgalanis@gmail.com>
2020-04-06 06:22:20 -0400

    load sorted grouped results at the component layer only

--- app/controllers/show.js
+++ app/controllers/show.js
@@ -7,31 +7,6 @@ export default Controller.extend({
   displayMobileToc: false,
   content: alias('model'),
 
-  groupedResults: computed('content.[]', function() {
-    let result = [];
-    this.content.forEach(function(item) {
-      let since = result.findBy('since', item.get('since'));
-      if(!since) {
-         result.pushObject(EmberObject.create({
-            since: item.get('since'),
-            contents: []
-         }));
-      }
-      result.findBy('since', item.get('since')).get('contents').pushObject(item);
-    });
-    return result;
-  }),
-
-  sortedGroupedResults: computed('groupedResults', function() {
-    let sorted = this.groupedResults.sort((a, b) => semverCompare(a.since, b.since));
-    let match = sorted[0].since.match(/Upcoming Features/)
-    if (match && match[0] == "Upcoming Features") {
-      let upComingFeatures = sorted.shift();
-      sorted.push(upComingFeatures);
-    }
-    return sorted;
-  }),
-
   version: computed('content', function() {
     let version = this.get('content.query.version');
     return version.match(/[0-9].*/)[0];
