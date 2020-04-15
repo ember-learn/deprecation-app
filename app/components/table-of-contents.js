dda4d0e5762472a58f49b20c6f3889eb77979cac
Jared Galanis <jaredgalanis@gmail.com>
2020-04-15 14:36:22 -0400

    make toc section a button

--- app/components/table-of-contents.js
+++ app/components/table-of-contents.js
@@ -1,29 +1,3 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { inject as service } from '@ember/service';
-import { later } from '@ember/runloop';
 
-export default Component.extend({
-    router: service(),
-    tocLevel: computed('level', function() {
-      return `level-${this.level}`;
-    }),
-    level: '1',
-
-    actions: {
-      navigateToLink(event) {
-        this.toggleProperty('displayMobileToc');
-
-        let anchor = event.target.href.split('/').lastObject.split('#').lastObject;
-
-        later(this, function() {
-          if (typeof document !== 'undefined') {
-            if (this.displayMobileToc) document.querySelector('body').classList.add('no-scroll');
-            if (!this.displayMobileToc) document.querySelector('body').classList.remove('no-scroll');
-            window.location.hash = anchor;
-            document.querySelector(`#${anchor}`).scrollIntoView();
-          }
-        }, 200);
-      }
-    }
-});
+export default Component.extend({});
---
+++ app/components/toc-section.js
@@ -0,0 +1,36 @@
+import Component from '@ember/component';
+import { computed, set } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { later } from '@ember/runloop';
+
+export default Component.extend({
+  router: service(),
+
+  tocLevel: computed('level', function() {
+    return `level-${this.level}`;
+  }),
+  level: '1',
+
+  init() {
+    this._super(...arguments);
+
+    set(this, 'open', this.displayMobileToc);
+  },
+
+  actions: {
+    navigateToLink(event) {
+      this.toggleProperty('displayMobileToc');
+
+      let anchor = event.target.href.split('/').lastObject.split('#').lastObject;
+
+      later(this, function() {
+        if (typeof document !== 'undefined') {
+          if (this.displayMobileToc) document.querySelector('body').classList.add('no-scroll');
+          if (!this.displayMobileToc) document.querySelector('body').classList.remove('no-scroll');
+          window.location.hash = anchor;
+          document.querySelector(`#${anchor}`).scrollIntoView();
+        }
+      }, 200);
+    }
+  }
+});
