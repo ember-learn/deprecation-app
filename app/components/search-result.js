c1a5a03eba0f8eaaba34e06746ccd01e6a192772
Jared Galanis <jaredgalanis@gmail.com>
2020-05-23 10:11:20 -0400

    convert search result component invocation to anglebrackets
    - use args

--- app/components/search-result.js
+++ app/components/search-result.js
@@ -1,25 +1,25 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { computed } from '@ember/object';
 
 export default class SearchResult extends Component {
-  @computed('result.path')
+  @computed('args.result.path')
   get project() {
-    return this.result.path.split('/')[0]
+    return this.args.result.path.split('/')[0];
   }
 
-  @computed('result.path')
+  @computed('args.result.path')
   get version() {
-    return this.result.path.split('/')[1]
+    return this.args.result.path.split('/')[1];
   }
 
-  @computed('result.path')
+  @computed('args.result.path')
   get sectionTitle() {
-    return this.result.path;
+    return this.args.result.path;
   }
 
-  @computed('result._highlightResult.headings.[]')
+  @computed('args.result._highlightResult.headings.[]')
   get remainingHeadings() {
-     let returnedRemainingHeadings = this.result._highlightResult.headings.slice(1);
+     let returnedRemainingHeadings = this.args.result._highlightResult.headings.slice(1);
      let lastHeading = returnedRemainingHeadings[returnedRemainingHeadings.length-1].value;
      if(lastHeading.indexOf('id:') === 0) {
       returnedRemainingHeadings.splice(-1, 1);
