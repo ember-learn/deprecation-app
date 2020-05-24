9430b6893dc4b42ed8517a93c4aaa7e8b8873d16
Jared Galanis <jaredgalanis@gmail.com>
2020-05-24 16:38:58 -0400

    improve loading experience
    - blocking loading at the component level while results are processed was causing a jarring experience on page load
    - hoists processing of results into a shared service relied upon by the routes
    - removes layout provider component pattern
    - removes fades which also causes some loading jank

--- app/routes/show.js
+++ app/routes/show.js
@@ -1,10 +1,21 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import RSVP from 'rsvp'
 
 export default class ShowRoute extends Route {
-  model(params) {
-    return this.store.query('content', {
+  @service resultProcessor;
+
+  async model(params) {
+    let query = await this.store.query('content', {
       path: params.project,
       version: params.version,
+    });
+
+    let sorted = await this.resultProcessor.processResults.perform(query);
+
+    return RSVP.hash({
+      query,
+      sorted
     });
   }
 }
