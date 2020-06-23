904459a53de8c6ebac61829086df8ae3ea06c091
ijlee2 <ijlee2@users.noreply.github.com>
2020-06-23 13:58:34 -0500

    Modernized application tests

--- tests/acceptance/visual-regression-test.js
+++ tests/acceptance/visual-regression-test.js
@@ -1,100 +1,74 @@
-import { click, isSettled, visit, waitFor } from "@ember/test-helpers";
-import { percySnapshot } from "ember-percy";
-import { setupApplicationTest } from "ember-qunit";
-import { module, test } from "qunit";
-import FakeTimers from '@sinonjs/fake-timers';
+import { click, visit } from '@ember/test-helpers';
+import percySnapshot from '@percy/ember';
+import { setupApplicationTest } from 'ember-qunit';
+import { module, test } from 'qunit';
 
-module("Acceptance | visual regression", function(hooks) {
+module('Acceptance | visual regression', function(hooks) {
   setupApplicationTest(hooks);
 
-  hooks.beforeEach(function() {
-    this.clock = FakeTimers.install({shouldAdvanceTime: true, advanceTimeDelta: 40});
-  });
-
-  hooks.afterEach(function() {
-    this.clock.uninstall();
-  });
 
   test('visit all the pages', async function(assert) {
+    // Homepage
     await visit('/');
-
     await percySnapshot('home-page');
 
+
+    // v1.x Ember
     await click('[data-test-ember-1-link] > a');
 
-    await waitFor('[data-test-deprecations-added-in]');
-    assert.dom('[data-test-deprecations-added-in]').includesText('Deprecations Added in Ember 1.x');
-    await isSettled();
+    assert.dom('[data-test-deprecations-added-in]')
+      .hasText('Deprecations Added in Ember 1.x');
 
-    this.clock.setTimeout(async function () {
-      await percySnapshot('ember-1.x');
-    }, 3);
-    this.clock.tick(3);
+    await percySnapshot('ember-1.x');
 
-    await click ('[data-test-main-deprecations-link]');
 
+    // v2.x Ember
+    await click('[data-test-main-deprecations-link]');
     await click('[data-test-ember-2-link] > a');
 
-    await waitFor('[data-test-deprecations-added-in]');
-    assert.dom('[data-test-deprecations-added-in]').includesText('Deprecations Added in Ember 2.x');
-    await isSettled();
+    assert.dom('[data-test-deprecations-added-in]')
+      .hasText('Deprecations Added in Ember 2.x');
 
-    this.clock.setTimeout(async function () {
-      await percySnapshot('ember-2.x');
-    }, 3);
-    this.clock.tick(3);
+    await percySnapshot('ember-2.x');
 
-    await click ('[data-test-main-deprecations-link]');
 
+    // v2.x Ember Data
+    await click('[data-test-main-deprecations-link]');
     await click('[data-test-ember-data-2-link] > a');
 
-    await waitFor('[data-test-deprecations-added-in]');
-    assert.dom('[data-test-deprecations-added-in]').includesText('Deprecations Added in Ember Data 2.x');
-    await isSettled();
+    assert.dom('[data-test-deprecations-added-in]')
+      .hasText('Deprecations Added in Ember Data 2.x');
 
-    this.clock.setTimeout(async function () {
-      await percySnapshot('ember-data-2.x');
-    }, 3);
-    this.clock.tick(3);
+    await percySnapshot('ember-data-2.x');
+ 
 
-
-    await click ('[data-test-main-deprecations-link]');
-
+    // v2.x Ember CLI
+    await click('[data-test-main-deprecations-link]');
     await click('[data-test-ember-cli-2-link] > a');
 
-    await waitFor('[data-test-deprecations-added-in]');
-    assert.dom('[data-test-deprecations-added-in]').includesText('Deprecations Added in Ember CLI 2.x');
-    await isSettled();
+    assert.dom('[data-test-deprecations-added-in]')
+      .hasText('Deprecations Added in Ember CLI 2.x');
 
-    this.clock.setTimeout(async function () {
-      await percySnapshot('ember-cli-2.x');
-    }, 3);
-    this.clock.tick(3);
+    await percySnapshot('ember-cli-2.x');
 
-    await click ('[data-test-main-deprecations-link]');
 
+    // v3.x Ember
+    await click('[data-test-main-deprecations-link]');
     await click('[data-test-ember-3-link] > a');
 
-    await waitFor('[data-test-deprecations-added-in]');
-    assert.dom('[data-test-deprecations-added-in]').includesText('Deprecations Added in Ember 3.x');
-    await isSettled();
+    assert.dom('[data-test-deprecations-added-in]')
+      .hasText('Deprecations Added in Ember 3.x');
 
-    this.clock.setTimeout(async function () {
-      await percySnapshot('ember-3.x');
-    }, 3);
-    this.clock.tick(3);
+    await percySnapshot('ember-3.x');
 
-    await click ('[data-test-main-deprecations-link]');
 
+    // v3.x Ember Data
+    await click('[data-test-main-deprecations-link]');
     await click('[data-test-ember-data-3-link] > a');
 
-    await waitFor('[data-test-deprecations-added-in]');
-    assert.dom('[data-test-deprecations-added-in]').includesText('Deprecations Added in Ember Data 3.x');
-    await isSettled();
+    assert.dom('[data-test-deprecations-added-in]')
+      .hasText('Deprecations Added in Ember Data 3.x');
 
-    this.clock.setTimeout(async function () {
-      await percySnapshot('ember-data-3.x');
-    }, 3);
-    this.clock.tick(3);
+    await percySnapshot('ember-data-3.x');
   });
 });
