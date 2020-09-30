8896cfe6f855f29e63596bd9c99eb8800ed26b81
ijlee2 <ijlee2@users.noreply.github.com>
2020-09-30 10:32:04 -0500

    Installed ember-cli-dependency-lint to prevent issues with updating packages in the future

---
+++ config/dependency-lint.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  allowedVersions: {
+    // TODO: The exception is due to ember-qunit using 2.6.0 and
+    // ember-styleguide using 2.7.1. When you upgrade these addons,
+    // please check if we no longer need to make this exception.
+    'ember-assign-polyfill': '2.6.0 || 2.7.1',
+  }
+};
