c6852b27334634d8ae2633014ab39aaf6432ae5f
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2020-10-01 19:28:43 -0500

    Updated development dependencies (#676)
    * Installed npm-run-all
    
    * Installed packages that a new Ember 3.21 app would have
    
    * Uninstalled ember-ajax
    
    * Updated ember-href-to and eslint-plugin-ember to the latest version
    
    * Updated ember-data to the latest version
    
    * Updated Node version to 12 in Travis CI
    
    * Installed ember-cli-dependency-lint to prevent issues with updating packages in the future
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
+'use strict';
+
 module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      legacyDecorators: true
+    }
   },
   plugins: [
     'ember'
@@ -15,8 +20,7 @@ module.exports = {
   env: {
     browser: true
   },
-  rules: {
-  },
+  rules: {},
   overrides: [
     // node files
     {
@@ -27,7 +31,8 @@ module.exports = {
         'testem.js',
         'blueprints/*/index.js',
         'config/**/*.js',
-        'lib/*/index.js'
+        'lib/*/index.js',
+        'server/**/*.js'
       ],
       parserOptions: {
         sourceType: 'script'
@@ -35,6 +40,13 @@ module.exports = {
       env: {
         browser: false,
         node: true
+      },
+      plugins: ['node'],
+      extends: ['plugin:node/recommended'],
+      rules: {
+        // this can be removed once the following is fixed
+        // https://github.com/mysticatea/eslint-plugin-node/issues/77
+        'node/no-unpublished-require': 'off'
       }
     }
   ]
