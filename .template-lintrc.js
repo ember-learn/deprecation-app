9cbc0f907d06ee2cb2d8e43bad21a194450f061f
Bert De Block <bert.deblock@bagaar.be>
2020-11-19 17:57:02 +0100

    resolve "link-rel-noopener" template lint errors

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -5,7 +5,6 @@ module.exports = {
 
   // TODO: Turn on a rule and fix errors
   rules: {
-    'link-rel-noopener': 'off',
     'no-curly-component-invocation': 'off',
     'no-implicit-this': 'off',
     'no-triple-curlies': 'off',
