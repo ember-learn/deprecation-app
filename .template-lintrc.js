ccaf27e2dba24e7cef366ffaccfbe53301a1ec65
Bert De Block <bert.deblock@bagaar.be>
2020-11-19 18:03:04 +0100

    resolve "no-implicit-this" template lint errors

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   // TODO: Turn on a rule and fix errors
   rules: {
     'no-curly-component-invocation': 'off',
-    'no-implicit-this': 'off',
     'no-triple-curlies': 'off',
   }
 };
