d47161eeca11dee0033e4b376052a4d2e1535f2e
Bert De Block <bert.deblock@bagaar.be>
2020-11-19 17:47:05 +0100

    resolve "no-action" template lint errors

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -6,7 +6,6 @@ module.exports = {
   // TODO: Turn on a rule and fix errors
   rules: {
     'link-rel-noopener': 'off',
-    'no-action': 'off',
     'no-curly-component-invocation': 'off',
     'no-implicit-this': 'off',
     'no-quoteless-attributes': 'off',
