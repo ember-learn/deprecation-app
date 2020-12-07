9b820fe19a30850170139b22c28e23af1d017b81
Ricardo Mendes <rokusu@gmail.com>
2020-12-07 15:42:49 +0000

    use class syntax for link-to component

--- app/components/link-to.js
+++ app/components/link-to.js
@@ -1,9 +1,9 @@
 import LinkComponent from '@ember/routing/link-component';
 
-export default LinkComponent.extend({
+export default class CustomLinkComponent extends LinkComponent {
   click() {
     if(window.scrollTo) {
       window.scrollTo(0,0);
     }
   }
-});
+}
